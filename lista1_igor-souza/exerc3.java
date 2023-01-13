09d15376615e3bea23578dda5ee9e4b7168666ba
Ígor Souza <igorrafael.jobs@gmail.com>
2023-01-12 21:20:19 -0300

    Update exerc3.java

--- lista1_igor-souza/exerc3.java
+++ lista1_igor-souza/exerc3.java
@@ -1,3 +1,8 @@
+/* 3. Faça um programa que lê o nome de um vendedor, o seu salário fixo e o total de
+vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha
+15% de comissão sobre suas vendas efetuadas, informar o total a receber no final
+do mês, com duas casas decimais. */
+
 import java.util.Scanner;
 
 public class exerc3 
