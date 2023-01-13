09301c08b84d0dd41a5e4dd07af4b3b23cc0fea2
Ígor Souza <igorrafael.jobs@gmail.com>
2023-01-12 21:19:00 -0300

    Update exerc10.java

--- lista1_igor-souza/exerc10.java
+++ lista1_igor-souza/exerc10.java
@@ -1,3 +1,7 @@
+/* 10. Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a
+em anos, meses e dias. Obs: Apenas para facilitar o cálculo, considere todo ano
+com 365 dias e todo mês com 30 dias. */
+
 import java.util.Scanner;
 
 class Main 
