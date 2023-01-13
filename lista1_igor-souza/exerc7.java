254984178a4cb645b7ad65cce1a2146886c32113
Ígor Souza <igorrafael.jobs@gmail.com>
2023-01-12 21:26:37 -0300

    Update exerc7.java

--- lista1_igor-souza/exerc7.java
+++ lista1_igor-souza/exerc7.java
@@ -1,3 +1,7 @@
+/* 7. Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis
+(cédulas) no qual o valor pode ser decomposto. As notas consideradas são de 100,
+50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e a relação de notas necessárias. */
+
 import java.util.Scanner;
 
 public class exerc7
